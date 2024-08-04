4c49f85570e98504d91c611f0a2e9ae7cbc2b862
Evan Jelley <jelleyevan@gmail.com>
2024-08-03 19:43:37 -0600

    Update settings.py

--- django_app/settings.py
+++ django_app/settings.py
@@ -145,6 +145,11 @@ else:
     STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/static/"
     MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/media/"
 
+    STATICFILES_DIRS = [f'https://{AWS_S3_CUSTOM_DOMAIN}' / 'static']
+    STATIC_ROOT = BASE_DIR / 'staticfiles'
+    MEDIA_ROOT = BASE_DIR / 'media'
+
+
     STORAGES = {
         "default": {
             "BACKEND": "storages.backends.s3boto3.S3Boto3Storage",
